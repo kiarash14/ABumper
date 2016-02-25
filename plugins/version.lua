51a36cd45774d1b247d2909e4d505c6af7fb7f0a
kiarash14 <omidbakhsh22@gmail.com>
2016-02-25 08:10:51 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,9 +1,22 @@
 do
 
 function run(msg, matches)
-  return 'Telegram Bot '.. VERSION .. [[ 
-  Checkout http://git.io/6jdjGg
-  GNU GPL v2 license.]]
+  return 'Bumper Bot '.. VERSION .. [[ 
+http://s7.picofile.com/file/8234722668/Bumper
+
+
+Sudo:
+@kiarash_gh1 [Manager]
+
+
+Id Bot
+@TeleBumper 
+
+Id Channel
+@Bumperch
+
+Group Support:
+https://telegram.me/joinchat/Bhf10QaYoGWmcv8fumYyMA
 end
 
 return {
